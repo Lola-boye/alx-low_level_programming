f201a3f3d0a4391f8f2c35dbed5e6f91f7bf6612
Lola-boye <boyedebimpe@gmail.com>
2022-09-09 15:34:39 -0700

    Prints numbers only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints numbers only
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
