0e2e5772931a346c297b8be87463a16da4c101c8
Lola-boye <boyedebimpe@gmail.com>
2022-09-09 16:49:59 -0700

    Prints combinations of two-digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combi of two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main|(void)
+{
+	int a;
+	int b;
+
+	a = 48;
+	b = 49;
+	while ((a < 57) && (b < 58))
+	{
+		putchar(a);
+		putchar(b);
+		if ((a == 56) && (b == 57))
+		{
+			putchar('\n');
+			a++;
+			b++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (b < 57)
+			{
+				b++;
+			}
+			else
+			{
+				a++;
+				b = a + 1;
+			}
+		}
+	}
+	return (0);
+}
