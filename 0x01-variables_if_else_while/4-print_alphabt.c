e36a5e8ff5e21335929b1ff96757d73cf558d179
Lola-boye <boyedebimpe@gmail.com>
2022-09-09 15:26:39 -0700

    Prints all lower cases except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: This prints all lower cases except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+	
+	l = 'a';
+	while (l <= 'z')
+	{
+		if ((l != 'e') && (l != 'q'))
+		{
+			putchar(l);
+			l++;
+		}
+		else
+		{
+			l++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
