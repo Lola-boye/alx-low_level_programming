249a1d4abc8e4f30829e32b2e3d05fa672fd9dd0
Lola-boye <boyedebimpe@gmail.com>
2022-09-09 17:06:12 -0700

    3-digit combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: This prints possible combi of 3-digit numbers
+ * REturn: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 48;
+	j = 49;
+	k = 50;
+	while ((i < 56) && (j < 57) && (k < 58))
+	{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		if ((i == 55) && (j == 5) && (k == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+			k++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (k < 57)
+			{
+				k++;
+			}
+			else if ((j < 56) && (k == 57))
+			{
+				k = j++ + 1;
+			}
+			else
+			{
+				j = i++ +1;
+				k = j + 1;
+			}
+		}
+	}
+	return (0);
+}
