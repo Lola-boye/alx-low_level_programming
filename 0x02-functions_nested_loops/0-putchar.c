2991012ad39504f54903646c50f7a45f86a93048
Lola-boye <103613554+Lola-boye@users.noreply.github.com>
2022-09-15 13:13:03 +0100

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,25 @@
 #include <studio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: This program Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c;
+	char s[] = "_putchar\n";
+
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
