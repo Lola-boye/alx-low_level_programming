24191a469d2204568c6f8dc437e401f57b58b85b
Lola-boye <boyedebimpe@gmail.com>
2022-09-16 16:49:24 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,16 @@
-return
+#include <stdio.h>>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Program.to print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
