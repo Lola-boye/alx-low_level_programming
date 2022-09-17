4260c151871ca1162ebc44ceb1c9446648c7d5d6
Lola-boye <boyedebimpe@gmail.com>
2022-09-16 17:58:54 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main -check the code for Holberton school students
+ * print_alphabet - check code
  *
  * Return: Always 0.
  */
 
 
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
