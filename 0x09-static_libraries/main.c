ed39e508726c094375778fddf22bc8f553d8e7fe
Lola-boye <boyedebpe@gmail.com>
2022-09-30 22:07:01 -0700

    commit

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,19 @@
 #include "main.h"
 
-int main(void)
+/**
+ * _puts - prints a string in stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchat('\n');
 }
