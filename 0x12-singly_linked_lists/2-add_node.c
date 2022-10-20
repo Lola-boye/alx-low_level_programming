99dbd49cd4abf380c840c1130ca369efbb7c3901
Lola-boye <boyedebpe@gmail.com>
2022-10-20 16:00:18 -0700

    node and free list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,46 +1,29 @@
-include "lists.h"
-#include <stdlib.h>
-#include <string.h>
+#include "lists.h"
 
 /**
- * _strlen - finds the length of a string
- * @str: string to find the length of
+ * add_node - function that adds a new node
+ * at the beginning of a list_t list.
  *
- * Return: length of string
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * add_node - adds a node to the beginning of a linked list
- * @head: double pointer to a lined list
- * @str: string to add to the new node
+ * @head: Parameter with the head node
+ * @str: parameter with the element str in
+ * the structure
  *
- * Return: pointer to the new node
+ * Return: Return the head of the linked list
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	if (str == NULL)
-		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
 	{
-		free(new);
 		return (NULL);
 	}
-	new->len = _strlen(new->str);
+
+	new->str = strdup(str);
+	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
-	return (new);
+
+	return (*head);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,23 +1,21 @@
-include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list
- * @head: pointer to the start of the list
+ * free_list - function that frees a list_t list.
  *
- * Return: void
+ * @head: parameter that point to the head of the
+ * linked list
  */
 void free_list(list_t *head)
 {
-	list_t *current, *next;
+	list_t *temp;
 
-	current = head;
-	while (current != NULL)
+	while (head)
 	{
-		next = current->next;
-		free(current->str);
-		free(current);
-		current = next;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
-
 }
+
