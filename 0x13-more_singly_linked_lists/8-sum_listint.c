4cc9a5336d6a97ea2c8062b4815fd9c1f88a0c73
Lola-boye <boyedebpe@gmail.com>
2022-10-25 17:13:31 -0700

    update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,27 +1,19 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a listint_t linked list.
- * @head: double pointer to the start of the list
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: pointer to the first node in the list
  *
- * Return: a pointer to the first node of the reversed list
+ * Return: sum of all the data (n) of a listint_t linked list
  */
-listint_t *reverse_listint(listint_t **head)
+int sum_listint(listint_t *head)
 {
-	listint_t *previous, *next;
+	int sum = 0;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
-	if ((*head)->next == NULL)
-		return (*head);
-	previous = NULL;
-	while (*head != NULL)
+	while (head != NULL)
 	{
-		next = (*head)->next;
-		(*head)->next = previous;
-		previous = *head;
-		*head = next;
+		sum += head->n;
+		head = head->next;
 	}
-	*head = previous;
-	return (*head);
+	return (sum);
 }
